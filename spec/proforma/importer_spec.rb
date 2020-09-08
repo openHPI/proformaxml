c86773c1a44486a81a73e94bf7efa7ee922039c9
Karol <git@koehn.pro>
2020-09-08 13:19:04 +0200

    refactor importer spec

--- spec/proforma/importer_spec.rb
+++ spec/proforma/importer_spec.rb
@@ -93,39 +93,10 @@ RSpec.describe Proforma::Importer do
       end
 
       context 'when test has any-configuration' do
-        # this should be fixed with a coherent solution after we adjusted the datamodel to support 100% proforma
-        let(:importer) { described_class.new(example_zip_file) }
-        let(:example_zip_file) { File.open('spec/support/examples/reverse_fixed.zip') }
-        let(:ref_task) do
-          build(
-            :task,
-            files: [],
-            tests: [
-              build(:test, files: [], id: '1', title: 'Compiler Test', test_type: 'java-compilation', configuration: nil),
-              build(:test,
-                    files: [build(
-                      :task_file, content: 'package reverse_task;foobar', id: '1', used_by_grader: true, visible: 'no',
-                                  binary: false, filename: 'reverse_task/DummyHelper.java'
-                    ), build(
-                      :task_file, content: 'package reverse_task;barfoo', id: '2', used_by_grader: true, visible: 'no',
-                                  binary: false, filename: 'reverse_task/MyStringTest.java'
-                    )],
-                    id: '2', title: 'Junit Test DummyHelper', test_type: 'unittest',
-                    configuration: {'framework' => 'JUnit', 'version' => '4.12', 'entry-point' => 'reverse_task.MyStringTest',
-                                    'type' => 'unittest'})
-            ],
-            model_solutions: [
-              build(:model_solution, id: '1', files: [build(
-                :task_file, content: 'package reverse_task;loremipsum', id: '3', used_by_grader: false, visible: 'delayed',
-                            binary: false, filename: 'reverse_task/MyString.java'
-              )])
-            ],
-            title: 'Reverse',
-            description: 'revert a string',
-            proglang: {name: 'java', version: '1.8'},
-            language: 'de',
-            uuid: 'be81c606-980a-4fdb-96b7-5fadefa464ed'
-          )
+        let(:task) do
+          build(:task, tests: build_list(:test, 1, test_type: 'unittest', configuration: {
+                                           'version' => '1.23', 'framework' => 'rspec', 'entry-point' => 'unit_file_spec.rb'
+                                         }))
         end
 
         it { is_expected.to be_an_equal_task_as ref_task }
