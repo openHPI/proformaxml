ae51197058d8665f9667d4ed49e669e6a58e152e
Sebastian Serth <Sebastian.Serth@hpi.de>
2022-03-01 15:38:04 +0100

    Fix rubocop offenses

--- spec/proforma_spec.rb
+++ spec/proforma_spec.rb
@@ -2,7 +2,7 @@
 
 RSpec.describe Proforma do
   it 'has a version number' do
-    expect(Proforma::VERSION).not_to be nil
+    expect(Proforma::VERSION).not_to be_nil
   end
 
   # it 'does something useful' do
