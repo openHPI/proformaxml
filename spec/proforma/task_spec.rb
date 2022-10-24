015fe2b5d4938bccfe1988cbc3ba6077d42a640a
Sebastian Serth <Sebastian.Serth@hpi.de>
2022-10-24 12:18:29 +0200

    Fix rubocop offenses

--- spec/proforma/task_spec.rb
+++ spec/proforma/task_spec.rb
@@ -8,43 +8,43 @@ RSpec.describe Proforma::Task do
   describe '#all_files' do
     subject { task.all_files }
 
-    let(:task) { build :task }
+    let(:task) { build(:task) }
 
     context 'when task has no files' do
       it { is_expected.to be_empty }
     end
 
     context 'when task has files' do
-      let(:task) { build :task, files: files }
-      let(:files) { build_list :task_file, 2 }
+      let(:task) { build(:task, files: files) }
+      let(:files) { build_list(:task_file, 2) }
 
       it { is_expected.to contain_exactly(*files) }
     end
 
     context 'when task has a model_solution' do
-      let(:task) { build :task, model_solutions: model_solutions }
+      let(:task) { build(:task, model_solutions: model_solutions) }
       let(:model_solutions) { [build(:model_solution, files: files)] }
-      let(:files) { build_list :task_file, 2 }
+      let(:files) { build_list(:task_file, 2) }
 
       it { is_expected.to contain_exactly(*files) }
     end
 
     context 'when task has a test' do
-      let(:task) { build :task, tests: tests }
+      let(:task) { build(:task, tests: tests) }
       let(:tests) { [build(:test, files: files)] }
-      let(:files) { build_list :task_file, 2 }
+      let(:files) { build_list(:task_file, 2) }
 
       it { is_expected.to contain_exactly(*files) }
     end
 
     context 'when task has files, model_solutions and tests' do
-      let(:task) { build :task, files: task_files, model_solutions: model_solutions, tests: tests }
+      let(:task) { build(:task, files: task_files, model_solutions: model_solutions, tests: tests) }
       let(:model_solutions) { [build(:model_solution, files: model_solution_files)] }
       let(:tests) { [build(:test, files: test_files)] }
 
-      let(:task_files) { build_list :task_file, 2 }
-      let(:model_solution_files) { build_list :task_file, 2 }
-      let(:test_files) { build_list :task_file, 2 }
+      let(:task_files) { build_list(:task_file, 2) }
+      let(:model_solution_files) { build_list(:task_file, 2) }
+      let(:test_files) { build_list(:task_file, 2) }
 
       it { is_expected.to contain_exactly(*(task_files + model_solution_files + test_files)) }
 
