23c9a428164938b98de34af606d878a4c4242625
Karol <git@koehn.pro>
2023-02-08 00:41:29 +0100

    add const containing mapping from namespace to filename

--- lib/proforma.rb
+++ lib/proforma.rb
@@ -14,7 +14,17 @@ require 'proforma/services/validator'
 require 'proforma/models/task'
 
 module Proforma
-  SCHEMA_FORMAT_PATH = File.join(File.dirname(File.expand_path(__FILE__)), '../assets/schemas/proforma_%s.xsd')
-  SCHEMA_VERSIONS = ['2.1', '2.0'].freeze
+  SCHEMA_PATH = File.join(File.dirname(File.expand_path(__FILE__)), '../assets/schemas')
+  SCHEMA_FORMAT_PATH = "#{SCHEMA_PATH}/proforma-%s.xsd".freeze
+  SCHEMA_VERSIONS = %w[2.1 2.0].freeze
+
+  TEST_TYPE_SCHEMA_NAMES = %w[checkstyle regexptest unittest].freeze
+  TEST_TYPE_SCHEMAS = {}.tap do |hash|
+    TEST_TYPE_SCHEMA_NAMES.each do |name|
+      path = SCHEMA_FORMAT_PATH % name
+      namespace = Nokogiri::XML(File.read(path)).xpath('xs:schema').first&.attributes&.dig('targetNamespace')&.value
+      hash[namespace] = name
+    end
+  end
   MAX_EMBEDDED_FILE_SIZE_KB = 50
 end
