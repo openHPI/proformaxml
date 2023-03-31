364d5e40746b6e0c39feaeb69c6443a27e4efd70
Sebastian Serth <Sebastian.Serth@hpi.de>
2023-04-01 00:34:24 +0200

    Disable focus filter for rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -27,7 +27,4 @@ RSpec.configure do |config|
   config.before(:suite) do
     FactoryBot.find_definitions
   end
-
-  config.filter_run focus: true
-  config.run_all_when_everything_filtered = true
 end
