8ca3ef5e1a8bc6b5d0c7f985dc1d34a4dc934cc9
Karol <git@koehn.pro>
2023-07-17 20:59:10 +0200

    apply and fix suggested changes to validator

--- lib/proforma/services/validator.rb
+++ lib/proforma/services/validator.rb
@@ -17,33 +17,10 @@ module Proforma
       @doc_schema_version ||= /^urn:proforma:v(.*)$/.match(@doc.namespaces['xmlns'])&.captures&.dig(0)
     end
 
-    def custom_test_config_validation
-      return [] if @doc.namespaces.values.none? {|ns| ns.start_with?('urn:proforma:tests:') }
-
-      validate_proforma_test_configuration
-    end
-
-    def validate_proforma_test_configuration
-      @doc.xpath('/xmlns:task/xmlns:tests/xmlns:test/xmlns:test-configuration').map do |test_config|
-        test_config.children.select do |c|
-          c.namespace&.href&.start_with?('urn:proforma:tests:')
-        end.map {|config| validate_proforma_config_node config }
-      end.flatten
-    end
-
-    def validate_proforma_config_node(config_node)
-      tmp_doc = node_as_doc_with_namespace(config_node)
-
-      return unless TEST_TYPE_SCHEMA_NAMES.include? config_node.name
-
-      schema = Nokogiri::XML::Schema(File.read(Proforma::SCHEMA_FORMAT_PATH % config_node.name))
-      schema.validate(Nokogiri::XML(tmp_doc.to_xml))
-    end
-
     def node_as_doc_with_namespace(config_node)
-      Nokogiri::XML(config_node.to_xml).tap do |doc|
-        doc.children.first.add_namespace_definition(config_node.namespace.prefix, config_node.namespace.href)
-      end
+      doc = Nokogiri::XML::Document.new
+      doc.add_child(config_node.dup)
+      doc
     end
 
     def validate
@@ -52,7 +29,24 @@ module Proforma
       version = @expected_version || doc_schema_version
       return ['version not supported'] unless SCHEMA_VERSIONS.include? version
 
-      Nokogiri::XML::Schema(File.open(SCHEMA_FORMAT_PATH % version)).validate(@doc) + custom_test_config_validation
+      # Both validations return an array of errors, which are empty if the validation was successful.
+      validate_task(version) + validate_test_configuration
+    end
+
+    def validate_task(version)
+      Nokogiri::XML::Schema(File.open(SCHEMA_FORMAT_PATH % version)).validate(@doc)
+    end
+
+    def validate_test_configuration
+      @doc.xpath('/xmlns:task/xmlns:tests/xmlns:test/xmlns:test-configuration').flat_map do |test_config|
+        test_config.children.flat_map do |config_node|
+          next [] unless config_node.namespace&.href&.start_with?('urn:proforma:tests:')
+          next [] unless TEST_TYPE_SCHEMA_NAMES.include? config_node.name
+
+          schema = Nokogiri::XML::Schema(File.read(SCHEMA_FORMAT_PATH % config_node.name))
+          schema.validate(node_as_doc_with_namespace(config_node))
+        end
+      end
     end
   end
 end
